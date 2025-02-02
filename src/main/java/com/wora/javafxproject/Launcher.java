3127858388e19de8344649cad5b88d87d67f9fd8
hamzalamin <hamzalamin80@gmail.com>
2025-02-02 23:54:02 +0100

    generate the jar file using artifact method

---
+++ src/main/java/com/wora/javafxproject/Launcher.java
@@ -0,0 +1,9 @@
+package com.wora.javafxproject;
+
+import javafx.application.Application;
+
+public class Launcher {
+    public static void main(String[] args){
+        Application.launch(HelloApplication.class, args);
+    }
+}
